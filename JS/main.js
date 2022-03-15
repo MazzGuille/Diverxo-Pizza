32481c0e0d7734309ea1b475a717cb64d4c3abc7
MazzGuille <mazzcode@gmail.com>
2022-03-15 19:18:53 -0300

    landing page 100% complete

---
+++ JS/main.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+
+sr.reveal('body',{
+    distance: '0px',
+    opacity: 0,
+    duration: 3500
+});
+
+
+sr.reveal('.logo-landing',{
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px',
+    delay: 1000,
+});
+
+sr.reveal('.pizza',{
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px',
+    delay: 1000,
+});
+
+sr.reveal('.title-landing',{
+    duration: 4500,
+    origin: 'top',
+    distance: '-100px',
+    delay: 2000,
+    rotate: {
+         x: -180
+    },
+    scale: 0.5
+});
+
+sr.reveal('.list-landing',{
+    duration: 3000,
+    origin: 'left',
+    distance: '-1000px',
+    delay: 5000,
+});
+
+sr.reveal('.sm-list',{
+    duration: 3000,
+    origin: 'right',
+    distance: '-1000px',
+    delay: 5000,
+});
+
+
+sr.reveal('.arrow',{
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+    delay: 5500,
+});
+
+
